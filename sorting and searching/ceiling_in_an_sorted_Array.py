32aaf46d14ceb54b72406c046037d851144b2bb2
Jahnvi sahni <121384173+jahnvisahni31@users.noreply.github.com>
2024-07-24 21:40:42 +0530

    Create ceiling_in_an_sorted_Array.py

---
+++ sorting and searching/ceiling_in_an_sorted_Array.py
@@ -0,0 +1,11 @@
+def ceilSearch(arr, low, high, x):
+   if x <= arr[low]:
+        return low
+   i = low
+    for i in range(high):
+        if arr[i] == x:
+            return i
+        if arr[i] < x and arr[i+1] >= x:
+            return i+1
+    return -1
+
